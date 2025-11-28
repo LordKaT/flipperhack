95b374dadc7569c3e3af9f63bba027c1366e943a
Nobody <non@non.non>
2025-11-28 11:48:43 -0500

    So that's how static inline works.

--- src/flipperhack_stats.h
+++ src/flipperhack_stats.h
@@ -19,7 +19,7 @@
 #define STAT_FLAG1 ((uint32_t)1u << STAT_FLAG1_BIT)
 #define STAT_FLAG2 ((uint32_t)1u << STAT_FLAG2_BIT)
 
-extern uint32_t stats_pack(
+static inline uint32_t stats_pack(
     uint8_t str,
     uint8_t dex,
     uint8_t con,
@@ -28,8 +28,33 @@ extern uint32_t stats_pack(
     uint8_t cha,
     bool flag1,
     bool flag2
-);
-extern uint8_t stats_get(uint32_t packed, uint8_t stat);
-extern void stats_set_flag(uint32_t* packed, uint32_t flag);
-extern void stats_clear_flag(uint32_t* packed, uint32_t flag);
-extern bool stats_get_flag(uint32_t packed, uint32_t flag);
+) {
+    uint32_t v = 0;
+    v |= ((uint32_t)(str & STAT_MASK) << (STAT_BITS * STATS_STR));
+    v |= ((uint32_t)(dex & STAT_MASK) << (STAT_BITS * STATS_DEX));
+    v |= ((uint32_t)(con & STAT_MASK) << (STAT_BITS * STATS_CON));
+    v |= ((uint32_t)(intl & STAT_MASK) << (STAT_BITS * STATS_INT));
+    v |= ((uint32_t)(wis & STAT_MASK) << (STAT_BITS * STATS_WIS));
+    v |= ((uint32_t)(cha & STAT_MASK) << (STAT_BITS * STATS_CHA));
+    v |= (flag1 ? STAT_FLAG1 : 0);
+    v |= (flag2 ? STAT_FLAG2 : 0);
+    return v;
+}
+
+static inline uint8_t stats_get(uint32_t packed, uint8_t stat) {
+    uint32_t shift = (uint32_t)stat * STAT_BITS;
+    return (uint8_t)((packed >> shift) & STAT_MASK);
+}
+
+static inline void stats_set_flag(uint32_t* packed, uint32_t flag) {
+    *packed |= flag;
+}
+
+static inline void stats_clear_flag(uint32_t* packed, uint32_t flag) {
+    *packed &= ~flag;
+}
+
+static inline bool stats_get_flag(uint32_t packed, uint32_t flag) {
+    return (packed & flag) != 0;
+}
+
